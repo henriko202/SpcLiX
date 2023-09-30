b277d13813b5e253d31c69d3072b37043fac93eb
Henriko Alberton <henriko.foz@gmail.com>
2023-09-30 18:25:54 -0300

    chore: changed api to /v1

--- server/src/main/config/setup-middleware.ts
+++ server/src/main/config/setup-middleware.ts
@@ -1,5 +1,5 @@
 import { bodyParser, contentType, cors, logFormat } from "@/main/middleware"
-import { type Express } from "express"
+import { Express } from "express"
 
 export default (app: Express): void => {
   app.use(bodyParser)
--- server/src/main/config/setup-routes-v1.ts
+++ server/src/main/config/setup-routes-v1.ts
@@ -4,6 +4,6 @@ import { Express, Router } from "express"
 
 export default (app: Express): void => {
   const router = Router()
-  app.use("/api", router)
+  app.use("/api/v1", router)
   router.get("/health-check", adaptRoute(HealthCheckController()))
 }
