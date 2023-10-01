6a3d430c283e4d317b2dda82dd5be6d76c079150
Henriko Alberton <henriko.foz@gmail.com>
2023-10-01 01:13:14 -0300

    refactor: changed a file name to reflect better... the implementation

--- server/src/main/factories/index.ts
+++ server/src/main/factories/index.ts
@@ -1,4 +1,4 @@
 export * from "./health-check-controller"
 export * from "./spacex-repository"
 export * from "./next-launch-controller"
-export * from "./last-launch-controller"
+export * from "./latest-launch-controller"
