f67a58d37964df82084e4bd81cf8c612595ddff4
Henriko Alberton <henriko.foz@gmail.com>
2023-10-09 17:06:56 -0300

    build: fixed the startup message to show HOST

--- server/src/main/server.ts
+++ server/src/main/server.ts
@@ -9,7 +9,8 @@ dotenv.config({
 })
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
+const host = process.env.HOST || "http://localhost"
 
 app.listen(port, () => {
-  console.log(`🚀 Server listening at localhost:${port}`)
+  console.log(`🚀 Server listening at ${host}:${port}`)
 })
