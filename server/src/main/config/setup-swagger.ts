63287ff1f87f2b5b6072328febf7c9fbd147dcdb
Henriko Alberton <henriko.foz@gmail.com>
2023-10-09 17:21:47 -0300

    build: change swagger url for production

--- server/src/main/config/setup-swagger.ts
+++ server/src/main/config/setup-swagger.ts
@@ -9,7 +9,11 @@ dotenv.config({
   path: path.resolve(__dirname, "..", "..", ".env"),
 })
 
-swaggerDocument.servers[0].url = `${process.env.HOST}:${process.env.PORT}/api/v1`
+if (process.env.ENVIRONMENT === "development") {
+  swaggerDocument.servers[0].url = `${process.env.HOST}:${process.env.PORT}/api/v1`
+} else {
+  swaggerDocument.servers[0].url = `${process.env.HOST}/api/v1`
+}
 
 export default (app: Express): void => {
   app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
